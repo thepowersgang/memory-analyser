c49bc069023f6ed35f4e68144cdcf590d35bfdec
John Hodge <tpg@mutabah.net>
2026-03-25 22:14:34 +0800

    Progressing on loading debug info

--- src/debug_info.rs
+++ src/debug_info.rs
@@ -2,7 +2,7 @@
 #[derive(Default)]
 pub struct DebugPool {
     functions: ::std::collections::HashMap<String,FunctionRecord>,
-    type_lookup: ::std::collections::HashMap<::gimli::UnitOffset,TypeRef>,
+    type_lookup: ::std::collections::HashMap< (usize, ::gimli::UnitOffset), TypeRef >,
     types: Vec<Option<Type>>,
     next_unit_index: usize,
 }
@@ -56,12 +56,44 @@ impl DebugPool {
             gimli::UnitType::Compilation => {
                 let abbr = debug_info.abbreviations(&u).unwrap();
 
-
-                #[derive(Debug)]
                 enum State {
                     Root,
-                    InFunction(String),
-                    InType(String),
+                    InType(String, TypeRef, Vec<CompositeField>),
+                    InFunction(String, FunctionRecord),
+                    // Should only exist underneath a `InFunction`
+                    FcnScope(PcRange),
+                }
+                impl ::std::fmt::Debug for State {
+                    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                        match self {
+                        Self::Root => write!(f, "Root"),
+                        Self::InType(name, ..) => f.debug_tuple("InType").field(name).finish(),
+                        Self::InFunction(name, ..) => f.debug_tuple("InFunction").field(name).finish(),
+                        Self::FcnScope(arg0) => f.debug_tuple("FcnScope").field(arg0).finish(),
+                        }
+                    }
+                }
+                fn parent_name(stack: &[State]) -> Option<&str> {
+                    for v in stack.iter().rev() {
+                        match v {
+                        State::Root => {},
+                        State::InFunction(n, ..) => return Some(n),
+                        State::FcnScope(..) => {},
+                        State::InType(n, _, _) => return Some(n),
+                        }
+                    }
+                    None
+                }
+                fn get_scoped_name(stack: &[State], prefix: &str, name: Option<&str>, ofs: ::gimli::UnitOffset) -> String {
+                    let mut full_name = parent_name(stack).unwrap_or_default().to_owned();
+                    full_name.push_str("::");
+                    full_name.push_str(prefix);
+                    use ::std::fmt::Write;
+                    match name {
+                    None => { let _ = write!(full_name, "@{}", ofs.0); },
+                    Some(n) => full_name.push_str(n),
+                    }
+                    full_name
                 }
                 {
                     let mut stack = vec![State::Root];
@@ -69,36 +101,57 @@ impl DebugPool {
                     while let Some(v) = ents.next_dfs().unwrap()
                     {
                         while stack.len() > v.depth as usize {
-                            match stack.pop() {
-                            Some(State::InFunction(ref n)) if n == "::main" && matches!(stack.last(), Some(State::Root)) => return,
+                            match stack.pop().unwrap() {
+                            State::InFunction(n, fr) => {
+                                println!("END fn {}", n);
+                                println!("{:?}", fr.variables);
+                                let was_main = n == "::main";
+                                self.functions.insert(n, fr);
+                                if was_main {
+                                    return
+                                }
+                            } 
+                            State::InType(name, ty, fields) => {
+                                println!("END type {}", name);
+                                self.types[ty.0] = Some(Type::Composite(CompositeType { name, fields }));
+                            }
                             _ => {},
                             }
                         }
                         if (v.depth as usize) > stack.len() {
                             continue ;
                         }
+                        fn get_pc_range(v: &gimli::DebuggingInformationEntry<::gimli::EndianSlice<::gimli::LittleEndian>>) -> PcRange {
+                            fn unwrap_addr(v: &::gimli::Attribute<::gimli::EndianSlice<::gimli::LittleEndian>>) -> u64 {
+                                match v.value() {
+                                ::gimli::AttributeValue::Addr(a) => a,
+                                ::gimli::AttributeValue::Udata(a) => a,
+                                _ => todo!("{v:?} : {:?}", v.value()),
+                                }
+                            }
+                            let at_lo = v.attr(::gimli::DW_AT_low_pc).map(|v| unwrap_addr(v));
+                            let at_hi = v.attr(::gimli::DW_AT_high_pc).map(|v| unwrap_addr(v));
+                            let ranges = v.attr(::gimli::DW_AT_ranges).map(|v| v.value());
+                            match (at_lo, at_hi, ranges)
+                            {
+                            (Some(lo), None, _) => PcRange { start: lo, end: lo },
+                            (Some(lo), Some(hi), _) => PcRange { start: lo, end: hi },
+                            (None, _, None) => PcRange { start: 0, end: 0 },
+                            (None, _, Some(r)) => PcRange { start: 0, end: 0 },
+                            }
+                        }
                         //println!("{} {:?} {:x?}", v.depth, stack, v);
                         match v.tag()
                         {
                         gimli::DW_TAG_subprogram => {
                             let name = get_name(&debug_info, &unit, v);
-                            println!("function name={:?} @ {}", name, v.depth);
-                            let mut full_name = String::new();
-                            for v in &stack {
-                                full_name.clear();
-                                match v {
-                                State::Root => {},
-                                State::InFunction(n) => full_name.push_str(n),
-                                State::InType(n) => full_name.push_str(n),
-                                }
-                                full_name.push_str("::");
-                            }
-                            use ::std::fmt::Write;
-                            match name {
-                            None => { let _ = write!(full_name, "@{}", v.offset.0); },
-                            Some(n) => full_name.push_str(n),
-                            }
-                            stack.push(State::InFunction(full_name));
+                            let full_name = get_scoped_name(&stack, "", name, v.offset);
+                            println!("fn {}: name={:?} @ {}", full_name, name, v.depth);
+                            let pc_range = get_pc_range(v);
+                            stack.push(State::InFunction(full_name, FunctionRecord {
+                                pc_range,
+                                variables: Default::default(),
+                            }));
                             continue;
                         },
 
@@ -107,23 +160,10 @@ impl DebugPool {
                             continue
                         },
                         gimli::DW_TAG_structure_type | gimli::DW_TAG_class_type => {
+                            let ty_ref = self.dwarf_type_ref(unit_index, v.offset);
                             let name = get_name(&debug_info, &unit, v);
-                            let mut full_name = String::new();
-                            for v in &stack {
-                                full_name.clear();
-                                match v {
-                                State::Root => {},
-                                State::InFunction(n) => full_name.push_str(n),
-                                State::InType(n) => full_name.push_str(n),
-                                }
-                                full_name.push_str("::");
-                            }
-                            use ::std::fmt::Write;
-                            match name {
-                            None => { let _ = write!(full_name, "struct@{}", v.offset.0); },
-                            Some(n) => { let _ = write!(full_name, "struct {}", n); },
-                            }
-                            stack.push(State::InType(full_name));
+                            let full_name = get_scoped_name(&stack, "struct ", name, v.offset);
+                            stack.push(State::InType(full_name, ty_ref, vec![]));
                             continue;
                         },
                         gimli::DW_TAG_enumeration_type => {
@@ -131,14 +171,10 @@ impl DebugPool {
                             continue;
                         },
                         gimli::DW_TAG_union_type => {
+                            let ty_ref = self.dwarf_type_ref(unit_index, v.offset);
                             let name = get_name(&debug_info, &unit, v);
-                            println!("> union_type: {:?}", name);
-                            let name = match name
-                                {
-                                Some(v) => format!("union {}", v),
-                                None => format!("union#{:?}", v.offset),
-                                };
-                            stack.push(State::InType(name));
+                            let full_name = get_scoped_name(&stack, "union ", name, v.offset);
+                            stack.push(State::InType(full_name, ty_ref, Vec::new()));
                             continue;
                         },
                         gimli::DW_TAG_const_type => {
@@ -166,12 +202,13 @@ impl DebugPool {
                             _ => {},
                             }
                         },
-                        State::InFunction(n) => {
+                        &State::InFunction(_, FunctionRecord { pc_range, .. }) | &State::FcnScope(pc_range) => {
                             match v.tag()
                             {
                             gimli::DW_TAG_lexical_block => {
                                 // TODO: Get code ranges, to be included in downstream `DW_TAG_variable` items
-                                stack.push(State::InFunction(n.clone()));
+                                let pc_range = get_pc_range(v);
+                                stack.push(State::FcnScope(pc_range));
                                 },
                             gimli::DW_TAG_GNU_template_parameter_pack => {},
                             gimli::DW_TAG_GNU_formal_parameter_pack => {},
@@ -193,7 +230,7 @@ impl DebugPool {
                                 let name = get_name(&debug_info, &unit, v);
                                 let loc = v.attr_value(gimli::DW_AT_location);
                                 let ty = v.attr_value(gimli::DW_AT_type);
-                                println!("{n} > Variable {:?} @ {:?} {:?}", name, loc, ty);
+                                println!(" > Variable {:?} @ {:?} {:?}", name, loc, ty);
                                 // Record the type and offset
                                 let pos = match loc {
                                     Some(v) => match v
@@ -203,8 +240,15 @@ impl DebugPool {
                                             VariablePosition::Expr(expression.0[..].to_owned())
                                         }
                                         gimli::AttributeValue::LocationListsRef(r) => {
-                                            let expression = debug_info.locations(&unit, r).unwrap().next().unwrap().unwrap().data;
-                                            VariablePosition::Expr(expression.0[..].to_owned())
+                                            let expression = debug_info.locations(&unit, r)
+                                                .unwrap()
+                                                .next()
+                                                .unwrap()
+                                                ;
+                                            match expression {
+                                            Some(expression) => VariablePosition::Expr(expression.data.0[..].to_owned()),
+                                            None => VariablePosition::OptimisedOut,
+                                            }
                                         }
                                         _ => todo!("Position: {:?}", v),
                                         },
@@ -221,14 +265,26 @@ impl DebugPool {
                                     },
                                     };
                                 // Get the current function (look up the stack) and add this variable, along with its contained scope
+                                if let Some(name) = name {
+                                    stack.iter_mut()
+                                        .filter_map(|v| match v { State::InFunction(_, fr) => Some(fr), _ => None })
+                                        .next()
+                                        .unwrap()
+                                        .variables.entry(name.to_owned())
+                                        .or_insert(VariableRecord { ty: ty.unwrap(), ranges: Vec::new() })
+                                        .ranges
+                                        .push(VariableRange { pc_range, position: pos });
+                                }
                                 },
                             
                             _ => {},
                             }
                         },
-                        State::InType(ty_name) => {
+                        State::InType(ty_name, _, _) => {
                             match v.tag()
                             {
+                            gimli::DW_TAG_GNU_template_template_param => {},
+
                             gimli::DW_TAG_template_type_parameter => {},
                             gimli::DW_TAG_template_value_parameter => {},
                             gimli::DW_TAG_inheritance => {},
@@ -242,7 +298,7 @@ impl DebugPool {
                                 let ty = v.attr(gimli::DW_AT_type);
                                 println!("> {ty_name}.field {name:?} @ {pos:?}: {ty:?} {v:?}");
                             },
-                            _ => todo!("{:x?}", v),
+                            _ => todo!("InType: {:x?}", v),
                             }
                         },
                         }
@@ -273,7 +329,7 @@ impl DebugPool {
     }
 
     fn dwarf_type_ref(&mut self, unit_index: usize, ofs: ::gimli::UnitOffset) -> TypeRef {
-        *self.type_lookup.entry(ofs)
+        *self.type_lookup.entry((unit_index, ofs))
             .or_insert_with(|| {
                 let rv = TypeRef(self.types.len());
                 self.types.push(None);
@@ -282,6 +338,7 @@ impl DebugPool {
     }
 }
 
+#[derive(Debug, Clone, Copy)]
 struct PcRange {
     start: u64,
     end: u64,
@@ -291,11 +348,17 @@ struct FunctionRecord {
     pc_range: PcRange,
     variables: ::std::collections::HashMap<String,VariableRecord>,
 }
+#[derive(Debug)]
 struct VariableRecord {
     ty: TypeRef,
-    pc_ranges: Vec<PcRange>,
-    stack_offset: u64,
+    ranges: Vec<VariableRange>,
 }
+#[derive(Debug)]
+struct VariableRange {
+    pc_range: PcRange,
+    position: VariablePosition,
+}
+#[derive(Debug)]
 enum VariablePosition {
     OptimisedOut,
     Fixed(u64),
@@ -304,6 +367,7 @@ enum VariablePosition {
 
 /// Reference to a type in the debug tree
 #[derive(Clone, Copy)]
+#[derive(Debug)]
 pub struct TypeRef(usize);
 
 pub enum Type {
@@ -328,6 +392,7 @@ impl CompositeType {
         self.fields.iter()
     }
 }
+#[derive(Debug)]
 pub struct CompositeField {
     pub offset: u64,
     pub name: String,
